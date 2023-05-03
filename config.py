5d46e90537383facbf4dc820d7f0883001e09615
possiblyHugo <alainhandzar@gmail.com>
2023-05-03 16:56:53 -0700

    Added:     + Checks for administrator     + Checks for role     + Checks for channel     + Config file

---
+++ config.py
@@ -0,0 +1,13 @@
+bot_settings = dict(
+    token = '***REMOVED***',
+    prefix = '!'
+)
+
+user_settings = dict(
+    allowed_role = ***REMOVED***,
+    admin_allowed = True
+)
+
+message_settings = dict(
+    suggestions_channel = "meals"
+)

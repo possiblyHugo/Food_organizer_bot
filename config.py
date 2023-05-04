65191c5ffac2730f8de61f6b2776a85d0b0a6287
possiblyHugo <100500500+possiblyHugo@users.noreply.github.com>
2023-05-03 18:48:42 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 bot_settings = dict(
-    token = 'MTEwMzQxMTk1MTc2NzM5MjI3Ng.GOZaRZ.pDDlJe1YwxkkaocmWtTsnLQLYi5x85W6FtZlFU',
+    token = 'BotTokenHere',
     prefix = '!',
     purge_amount = 100
 )
 
 user_settings = dict(
-    allowed_role = 1102301293999046776,
+    allowed_role = role_id_here,
     admin_allowed = True
 )
 
 message_settings = dict(
     suggestions_channel = "meals",
-    finalize_channel_id = 1102301729434898563
+    finalize_channel_id = channel_id_here
 )
 
 reaction_settings = dict(

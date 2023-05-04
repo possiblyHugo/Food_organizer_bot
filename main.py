8a2eef6e40e5a10ca2fcf2441077fb706565fb1b
possiblyHugo <alainhandzar@gmail.com>
2023-05-03 17:08:46 -0700

    Added:     + Message listener     + BotAction class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import config
+import re
 from discord.ext import commands
 
 description = '''A discord bot that assists families decide on what foods to eat during the week. This works better in hand with the paprika app.'''
@@ -11,12 +12,20 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix=config.bot_settings['prefix'], description=description, intents=intents)
 
+class BotActions:
+    def __init__(self, message):
+        self.message = message
+
+    def react():
+        self.message.add_reaction()
 
 # Functions
 def is_admin():
     def predicate(ctx):
         return config.user_settings['admin_allowed'] and commands.has_permissions(administrator=True)
     return commands.check(predicate)
+
+
 
 
 # Commands
@@ -29,8 +38,8 @@ async def on_ready():
 @bot.event
 @commands.check_any(commands.has_role(config.user_settings['allowed_role']), is_admin()) 
 async def on_message(message):
-    if message.channel.TextChannel.name == config.message_settings['suggestions_channel']:
-
+    if message.channel.name == config.message_settings['suggestions_channel']:
+        await add_reaction
 
 
 
